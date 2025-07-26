1ea182ddb2394d5d7d2dd1277650b5afb230f000
Alex Balak <alex.balak@outlook.com>
2025-07-26 23:51:16 +0200

    working GET notes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
 
 export default function App() {
-  return <div>App</div>
+  const [notes, setNotes] = useState([])
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get("/api/notes")
+      setNotes(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+  return <div>
+    <h1>Notes</h1>
+    <ul>
+      {notes.map(note => (
+        <li key={note.id}>{note.title}</li>
+      ))}
+    </ul>    
+  </div>
 }
